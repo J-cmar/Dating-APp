e102ff18c7fd6f803a30d95fb024184a2dee7a9c
Mbee0 <mbee@cpp.edu>
2023-12-03 15:48:27 -0800

    some stuff for like and dislike

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -4,12 +4,13 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyDrC0eLL336PUqVZQGQaO4Y4KGj7noN3-w",
-	authDomain: "dating-app-43ccb.firebaseapp.com",
-	projectId: "dating-app-43ccb",
-	storageBucket: "dating-app-43ccb.appspot.com",
-	messagingSenderId: "476291162706",
-	appId: "1:476291162706:web:b0cfca9978453873b7358b",
+	apiKey: "AIzaSyCs547ZitkI5-IVY-1xGhdzkkOdRX3hO-c",
+	authDomain: "datingapptrial.firebaseapp.com",
+	projectId: "datingapptrial",
+	storageBucket: "datingapptrial.appspot.com",
+	messagingSenderId: "346468110355",
+	appId: "1:346468110355:web:3ff59ca6519d6e3822afe6",
+	measurementId: "G-YFQYBQ2J3V"
 };
 
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,18 @@
 import { useState } from 'react';
-import { collection, doc, setDoc,getDoc, query, where, onSnapshot, getFirestore, getDocs } from "firebase/firestore";
+import { collection, doc, setDoc, getDoc, query, where, onSnapshot, getFirestore, getDocs, updateDoc, arrayUnion } from "firebase/firestore";
 import { db, storage, auth } from "../firebase";
 import { AuthContext } from "../context/AuthContext";
 import { async } from '@firebase/util';
 import { useContext, useEffect } from 'react';
 import account from '../img/blank-avatar.png'
-import {useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 
-const getUserLocation = async (uid) =>{
-
-    const userRef = doc(db, 'users', uid);
-    const userDoc = await getDoc(userRef);
-    return userDoc.data()?.location || null;
-    
+// grabs the user location
+const getUserLocation = async (uid) => {
+  const userRef = doc(db, 'users', uid);
+  const userDoc = await getDoc(userRef);
+  console.log("location 2: " + userDoc.data().location);
+  return userDoc.data()?.location || null;
 }
 
 const returnUsers = async (location, uid) => {
@@ -21,54 +21,130 @@ const returnUsers = async (location, uid) => {
 
   const querySnapshot = await getDocs(q);
   const users = querySnapshot.docs
-  .filter(doc => doc.id !== uid)
-  .map(doc => ({ ...doc.data(), uid: doc.id }));
+    .filter(doc => doc.id !== uid)
+    .map(doc => ({ ...doc.data(), uid: doc.id }));
   return users;
 };
+
+const getUserLiked = async (uid) => {
+  const userRef = doc(db, 'users', uid);
+  const userDoc = await getDoc(userRef);
+  return userDoc.data()?.liked || null;
+}
+
+const getUserDisliked = async (uid) => {
+  const userRef = doc(db, 'users', uid);
+  const userDoc = await getDoc(userRef);
+  return userDoc.data()?.disliked || null;
+}
 
 const Home = () => {
   const [users, setUsers] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [location, setLocation] = useState(null);
-
-
   const { currentUser } = useContext(AuthContext);
+  const [likedCurrent, setCurrentLiked] = useState([]);
 
-  if(currentUser == null){
+  if (currentUser == null) {
     const navigate = useNavigate();
-    navigate("/")
-  }else{
-    console.log('not null')
+    navigate("/login")
+  } else {
+    // console.log('not null')
   }
+
   useEffect(() => {
     const fetchData = async () => {
       const userLocation = await getUserLocation(currentUser.uid);
-      setLocation(userLocation);
-
       if (userLocation) {
         const usersInLocation = await returnUsers(userLocation, currentUser.uid);
         setUsers(usersInLocation);
       }
     };
-
     fetchData();
   }, [currentUser.uid]);
 
   useEffect(() => {
-    // Update UI based on the current user
-    const currentUser = users[currentIndex];
-    if (currentUser) {
-      document.getElementById("img").src = currentUser.viewPhotoURL || account;
-      document.getElementById("name").textContent = "Name: " + currentUser.displayName || '';
-      document.getElementById("age").textContent = "Age: "+currentUser.age || '';
-      document.getElementById("bio").textContent = "Bio: "+currentUser.bio || '';
-      document.getElementById("location").textContent = "Location: "+currentUser.location || '';
+    generateUserViewed();
+  });
+
+  const generateUserViewed = () => {
+    const currentUserViewed = users[currentIndex];
+    if (currentUserViewed) {
+      document.getElementById("img").src = currentUserViewed.viewPhotoURL || account;
+      document.getElementById("name").textContent = "Name: " + currentUserViewed.displayName || '';
+      document.getElementById("age").textContent = "Age: " + currentUserViewed.age || '';
+      document.getElementById("bio").textContent = "Bio: " + currentUserViewed.bio || '';
+      document.getElementById("location").textContent = "Location: " + currentUserViewed.location || '';
     }
-  }, [users, currentIndex]);
-  
-  const nextUser = () => {
+  }
+
+  const hitLike = async () => {
+    // grab the user that they are currently viewing
+    const user = users[currentIndex];
+    const userLiked = await getUserLiked(user.uid);
+    const userDisliked = await getUserDisliked(user.uid);
+
+    // see if this person has like the current user before
+    if (userLiked != null && userLiked.includes(currentUser.uid)) {
+      // they have liked this user before
+      // check whether the group(chats in firestore) exists, if not create
+      const combinedId =
+        currentUser.uid > user.uid
+          ? currentUser.uid + user.uid
+          : user.uid + currentUser.uid;
+      try {
+        const res = await getDoc(doc(db, "chats", combinedId));
+
+        if (!res.exists()) {
+          //create a chat in chats collection
+          await setDoc(doc(db, "chats", combinedId), { messages: [] });
+
+          //create user chats for current user and other user since they matched
+          await updateDoc(doc(db, "userChats", currentUser.uid), {
+            [combinedId + ".userInfo"]: {
+              uid: user.uid,
+              displayName: user.displayName,
+              photoURL: user.photoURL,
+            },
+            [combinedId + ".date"]: serverTimestamp(),
+          });
+
+          await updateDoc(doc(db, "userChats", user.uid), {
+            [combinedId + ".userInfo"]: {
+              uid: currentUser.uid,
+              displayName: currentUser.displayName,
+              photoURL: currentUser.photoURL,
+            },
+            [combinedId + ".date"]: serverTimestamp(),
+          });
+        }
+      } catch (err) { }
+    } else if (userDisliked != null && userDisliked.includes(currentUser.uid)) {
+      console.log("did not match!");
+    } else {
+
+      const currentRef = doc(db, "users", currentUser.uid);
+      const userDoc = await getDoc(currentRef);
+      if (!userDoc.data().liked.includes(user.uid)) {
+        // Add a new user to the "liked" array field if it isn't already in there
+        await updateDoc(currentRef, {
+          liked: arrayUnion(user.uid)
+        });
+      }
+    };
+    // jasons thing that works :D
     setCurrentIndex((prevIndex) => (prevIndex + 1) % users.length);
-  };
+  }
+
+  // if the current user hits the dislike button, we want to add them to the dislike pile
+  const hitDislike = async () => {
+    const user = users[currentIndex];
+    const currentRef = doc(db, "users", currentUser.uid);
+    // Atomically add a new user to the "disliked" array field.
+    await updateDoc(currentRef, {
+      disliked: arrayUnion(user.uid)
+    });
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % users.length);
+  }
 
   return (
     // {changeUser}
@@ -76,22 +152,22 @@ const Home = () => {
       <div className="container">
         <h1>this is the new home</h1>
         <h1>WELCOME TO FUMBLE</h1>
-        <br/>
-        <br/>
-        <hr/>
+        <br />
+        <br />
+        <hr />
         <div>
-          <img id="img" alt="" src={account} height={100}/>
-        <p id="name"></p>
-        <p id="age"></p>
-        <p id="bio"></p>
-        <p id="location"></p>
+          <img id="img" alt="" src={account} height={100} />
+          <p id="name"></p>
+          <p id="age"></p>
+          <p id="bio"></p>
+          <p id="location"></p>
 
-        
-        <button onClick={nextUser}>Like</button>
-        <button onClick={nextUser}>Dislike</button>
+
+          <button onClick={hitLike}>Like</button>
+          <button onClick={hitDislike}>Dislike</button>
         </div>
-        
-        </div>
+
+      </div>
     </div>
   )
 }
